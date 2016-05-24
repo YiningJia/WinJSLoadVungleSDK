d1721dc17f590411f50285b908b8917dff10a245
Yining <yining.jia@vungle.com>
2016-05-24 15:45:22 +0800

    first commit

---
+++ CSharpWrapper/Class1.cs
@@ -0,0 +1,22 @@
+﻿using VungleSDK;
+using System;
+
+
+namespace CSharpWrapper
+{
+    public sealed class CSharpBridge
+    {
+        static VungleAd globalAd = null;
+
+        public string TestCSharpFunctionCall()
+        {
+            return "This is result returned from CSharp function";
+        }
+
+        public string callVungleSDK()
+        {
+            globalAd = AdFactory.GetInstance("vungleTest", new VungleSDKConfig() { });
+            return string.Empty;
+        }
+    }
+}
---
+++ WinJSLoadVungleSDK/bin/x86/Debug/AppX/js/default.js
@@ -0,0 +1,41 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+				// TODO: This application has been newly launched. Initialize your application here.
+			} else {
+				// TODO: This application was suspended and then terminated.
+				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+			}
+			args.setPromise(WinJS.UI.processAll());
+
+			document.getElementById("callCSharpTest").addEventListener("click", cSharpTestButtonHandler);
+
+			document.getElementById("callVungleSDK").addEventListener("click", vungleSDKButtonHandler);
+		}
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+	function cSharpTestButtonHandler(eventInfo) {
+	    document.getElementById("testInput").innerText = new CSharpWrapper.CSharpBridge().testCSharpFunctionCall();
+
+	}
+
+	function vungleSDKButtonHandler(eventInfo) {
+	    this.vungleSDK = new CSharpWrapper.CSharpBridge().callVungleSDK();
+	}
+
+	app.start();
+})();
